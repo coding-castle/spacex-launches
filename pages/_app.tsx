9cf04f373ba9bb23de6e6db75a8ffff75a6d3b52
patrickgoeler <patrickvongoeler@gmail.com>
2020-12-27 15:25:12 +0100

    Add Apollo Client with working SSR example

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,14 @@
+import { ApolloProvider } from "@apollo/client"
 import { AppProps } from "next/app"
 import "tailwindcss/tailwind.css"
+import { useApollo } from "../lib/ApolloClient"
 
 export default function App({ Component, pageProps }: AppProps) {
+	const apolloClient = useApollo(pageProps)
 	const getLayout = (Component as any).getLayout || ((page: any) => page)
-	return getLayout(<Component {...pageProps}></Component>)
+	return (
+		<ApolloProvider client={apolloClient}>
+			{getLayout(<Component {...pageProps}></Component>)}
+		</ApolloProvider>
+	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,28 @@
 import { getLayout } from "../components/Layout/Base"
+import PostList, { allPostsQueryVars, ALL_POSTS_QUERY } from "../components/PostList"
+import { addApolloState, initializeApollo } from "../lib/ApolloClient"
 
 export default function Index() {
-	return <h1 className="text-red-500">Hello Next.js 👋</h1>
+	return (
+		<>
+			<h1 className="text-red-500">Hello Next.js 👋</h1>
+			<PostList />
+		</>
+	)
 }
 
 ;(Index as any).getLayout = getLayout
+
+export async function getStaticProps() {
+	const apolloClient = initializeApollo()
+
+	await apolloClient.query({
+		query: ALL_POSTS_QUERY,
+		variables: allPostsQueryVars,
+	})
+
+	return addApolloState(apolloClient, {
+		props: {},
+		revalidate: 1,
+	})
+}
