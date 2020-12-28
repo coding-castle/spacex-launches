c056266b46261cd28a4e1977f7585d034be8e5de
patrickgoeler <patrickvongoeler@gmail.com>
2020-12-28 18:15:27 +0100

    Fix Icon layout shift

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,11 @@
 import { ApolloProvider } from "@apollo/client"
+import { config } from "@fortawesome/fontawesome-svg-core"
 import { AppProps } from "next/app"
 import "tailwindcss/tailwind.css"
+import "@fortawesome/fontawesome-svg-core/styles.css"
 import { useApollo } from "../lib/ApolloClient"
+
+config.autoAddCss = false
 
 export default function App({ Component, pageProps }: AppProps) {
 	const apolloClient = useApollo(pageProps)
